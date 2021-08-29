21a6759a5eeac1f8e07acd33f9c7a04fe030b205
cvinay92 <vinaytraker@gmail.com>
2021-08-29 16:53:47 +0530

    first commit

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is my Component
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eligendi
+        distinctio minima fuga dolor ab earum cum minus deserunt voluptates
+        maiores voluptatem harum ipsa, est enim. Quo sunt magni error assumenda
+        optio nam illo.
+      </p>
+    </div>
+  );
+};
